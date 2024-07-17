f64b51c20cdaea8126acaa8c347fdde0d3dbcf82
RyzenMu <muruga13in2022@gmail.com>
2024-07-17 16:36:44 +0530

    higher order functions

--- starter files/index.js
+++ starter files/index.js
@@ -5,3 +5,19 @@ function clickFunction(){
 buttons.forEach((item) => {
     item.addEventListener('click', clickFunction);
 })
+
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+function mul(n1, n2) {
+    return n1 * n2;
+}
+
+// higher order function
+function calc(n1, n2, operator) {
+    return operator(n1, n2);
+}
+
+console.log(calc(3, 4, add));
+console.log(calc(3, 4, mul));
